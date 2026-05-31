c0827bd6ebda6482cc5bb0b66328f897aaa53904
Uriy-T <briner_asterios@mail.ru>
2026-05-31 15:15:19 +0400

    Модуль src/widget.py. Добавлена реализация функции get_date.

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,14 @@
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
-def mask_account_card(requisite: str) -> str|None:
+
+def mask_account_card(requisite: str) -> str | None:
+    """Функция принимает на вход реквизит клиента банка и в зависимости от типа применят соответствующий механизм
+    маскИрования данных.
+
+    :param requisite: принимает строку с данными реквизита клиента
+    :return: возвращает строку с замаскИрованными данными в зависимости от типа клиента. В случае подачи данных в
+    невалидном формате или реквизита не известного типа, выводит соответствующее сообщение.
+    """
     if requisite and isinstance(requisite, str):
         requisite_type = ' '.join(requisite.split()[:-1])
         requisite_number = requisite.split()[-1]
@@ -15,5 +23,11 @@ def mask_account_card(requisite: str) -> str|None:
         return 'Предоставлены неверные данные. Проверьте правильность ввода данных'
 
 
+def get_date(date: str) -> str:
+    """Функция преобразует дату в формате ISO 8601 в дату в формате ДД.ММ.ГГГГ.
 
-
+    :param date: принимает на вход строчное значение даты в формате ISO 8601
+    :return: возвращает строчное значение даты в формате ДД.ММ.ГГГГ
+    """
+    date_elems = reversed(date.split('T')[0].split('-'))
+    return '.'.join(date_elems)
