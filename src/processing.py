ddffbc16dece44dfbead14e97e5b73673d619b86
Uriy-T <briner_asterios@mail.ru>
2026-06-11 01:21:53 +0400

    Функциям filter_by_state и sort_by_date добавлена документация (docstring).

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,15 @@
 def filter_by_state(data_for_filter: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    """
+    Функция-фильтр, отделяющая объекты с данными из общего набора в соответствии со значением параметра state.
+    :param data_for_filter: список объектов (словарей), на основе которых проводится фильтрация.
+    :param state: тип состояния, по которому проводится фильтрация объектов из параметра data_for_filter.
+    В базовой версии доступны состояния 'EXECUTED' и 'CANCELED'. Значение параметра по умолчанию: 'EXECUTED'.
+
+    :return:
+    - При подаче на вход валидных данных, возвращает список объектов (словарей) обладающих указанным значением ключа state.
+    - При подаче на вход невалидных данных возвращает исключения соответствующие варианту неверного ввода.
+    """
+
     available_states = ['EXECUTED', 'CANCELED']
 
     if not isinstance(data_for_filter, list):
@@ -17,6 +28,18 @@ def filter_by_state(data_for_filter: list[dict], state: str = 'EXECUTED') -> lis
 
 
 def sort_by_date(data_for_sort: list[dict], sort_order_reverse: bool = True) -> list[dict]:
+    """
+    Функция сортирует полученный набор объектов (словарей) по значению параметра date.
+    :param data_for_sort: список объектов (словарей), которые нужно отсортировать
+    :param sort_order_reverse: параметр управления направлением сортировки по убыванию/возрастанию.
+    По умолчанию сортировка происходит по убыванию (True)
+
+    :return:
+    - При подаче на вход валидных данных, возвращает список объектов (словарей) отсортированный по дате (date), в заданном
+    направлении.
+    - При подаче на вход невалидных данных возвращает исключения соответствующие варианту неверного ввода.
+    """
+
     if not isinstance(data_for_sort, list):
         raise TypeError(f'data_for_sort должен быть list, получен тип: {type(data_for_sort).__name__}')
 
@@ -27,4 +50,3 @@ def sort_by_date(data_for_sort: list[dict], sort_order_reverse: bool = True) -> 
         raise TypeError(f'state должен быть str, получен тип: {type(sort_order_reverse).__name__}')
 
     return sorted(data_for_sort, key=lambda x: x['date'], reverse=sort_order_reverse)
-
