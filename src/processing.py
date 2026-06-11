bbe1a0979945b06ddae598daec2f6cdb32f34cad
Uriy-T <briner_asterios@mail.ru>
2026-06-11 22:28:36 +0400

    Адаптация кода под требования линтеров

--- src/processing.py
+++ src/processing.py
@@ -1,29 +1,33 @@
-def filter_by_state(data_for_filter: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(data_for_filter: list[dict],
+                    state: str = "EXECUTED") -> list[dict]:
     """
-    Функция-фильтр, отделяющая объекты с данными из общего набора в соответствии со значением параметра state.
-    :param data_for_filter: список объектов (словарей), на основе которых проводится фильтрация.
-    :param state: тип состояния, по которому проводится фильтрация объектов из параметра data_for_filter.
-    В базовой версии доступны состояния 'EXECUTED' и 'CANCELED'. Значение параметра по умолчанию: 'EXECUTED'.
+    Функция отделяет объекты с данными из общего набора по значению rest.
+    :param data_for_filter: список словарей, для фильтрации.
+    :param state: тип состояния, по которому проводится фильтрация.
 
     :return:
-    - При подаче на вход валидных данных, возвращает список объектов (словарей),
+    - При подаче на вход валидных данных, возвращает список словарей,
     обладающих указанным значением ключа state.
-    - При подаче на вход невалидных данных возвращает исключения соответствующие варианту неверного ввода.
+    - При подаче на вход невалидных данных возвращает исключения,
+    соответствующие случаю неверного ввода.
     """
 
     available_states = ["EXECUTED", "CANCELED"]
 
     if not isinstance(data_for_filter, list):
-        raise TypeError(f"data_for_filter должен быть list, получен тип: {type(data_for_filter).__name__}")
+        raise TypeError(f"data_for_filter должен быть list."
+                        f"Получен тип: {type(data_for_filter).__name__}")
 
     if not all(isinstance(item, dict) for item in data_for_filter):
         raise TypeError("Все элементы data_for_filter должен быть dict")
 
     if not isinstance(state, str):
-        raise TypeError(f"state должен быть str, получен тип: {type(state).__name__}")
+        raise TypeError(f"state должен быть str."
+                        f"Получен тип: {type(state).__name__}")
 
     if state not in available_states:
-        raise ValueError(f"Значения state нет в списке доступных состояний. Доступны: {available_states}")
+        raise ValueError(f"Значения state нет в списке доступных состояний."
+                         f"Доступны: {available_states}")
 
     for i, item in enumerate(data_for_filter):
         if "state" not in item:
@@ -32,30 +36,34 @@ def filter_by_state(data_for_filter: list[dict], state: str = "EXECUTED") -> lis
     return [item for item in data_for_filter if item["state"] == state]
 
 
-def sort_by_date(data_for_sort: list[dict], sort_order_reverse: bool = True) -> list[dict]:
+def sort_by_date(data_for_sort: list[dict],
+                 sort_order: bool = True) -> list[dict]:
     """
-    Функция сортирует полученный набор объектов (словарей) по значению параметра date.
-    :param data_for_sort: список объектов (словарей), которые нужно отсортировать
-    :param sort_order_reverse: параметр управления направлением сортировки по убыванию/возрастанию.
+    Функция сортирует полученный набор словарей по значению параметра date.
+    :param data_for_sort: список словарей для сортировки.
+    :param sort_order: контролирует направление сортировки.
     По умолчанию сортировка происходит по убыванию (True)
 
     :return:
-    - При подаче на вход валидных данных, возвращает список объектов (словарей) отсортированный по дате (date),
+    - При вводе валидных данных, возвращает отсортированный список словарей,
     в заданном направлении.
-    - При подаче на вход невалидных данных возвращает исключения соответствующие варианту неверного ввода.
+    - При вводе невалидных данных возвращает исключения,
+    соответствующие случаю неверного ввода.
     """
 
     if not isinstance(data_for_sort, list):
-        raise TypeError(f"data_for_sort должен быть list, получен тип: {type(data_for_sort).__name__}")
+        raise TypeError(f"data_for_sort должен быть list."
+                        f"Получен тип: {type(data_for_sort).__name__}")
 
     if not all(isinstance(item, dict) for item in data_for_sort):
         raise TypeError("Все элементы data_for_filter должен быть dict")
 
-    if not isinstance(sort_order_reverse, bool):
-        raise TypeError(f"state должен быть str, получен тип: {type(sort_order_reverse).__name__}")
+    if not isinstance(sort_order, bool):
+        raise TypeError(f"state должен быть str."
+                        f"Получен тип: {type(sort_order).__name__}")
 
     for i, item in enumerate(data_for_sort):
         if "date" not in item:
             raise KeyError(f"Элемент с индексом {i} не содержит ключ 'date'")
 
-    return sorted(data_for_sort, key=lambda x: x["date"], reverse=sort_order_reverse)
+    return sorted(data_for_sort, key=lambda x: x["date"], reverse=sort_order)
--- src/widget.py
+++ src/widget.py
@@ -4,25 +4,28 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(requisite: str) -> str | None:
-    """Функция принимает на вход реквизит клиента банка и в зависимости от типа применят соответствующий механизм
-    маскИрования данных.
+    """Функция принимает на вход реквизит клиента банка.
+    Применят механизм маскИрования данных, соответствующий типу реквизита.
 
     :param requisite: принимает строку с данными реквизита клиента
-    :return: возвращает строку с замаскИрованными данными в зависимости от типа клиента. В случае подачи данных в
-    невалидном формате или реквизита не известного типа, выводит соответствующее сообщение.
+    :return: возвращает замаскИрованную строку с данными реквизита.
+
+    В случае ввода в невалидных, выводит соответствующее ситуации сообщение.
     """
     if requisite and isinstance(requisite, str):
         requisite_type = " ".join(requisite.split()[:-1])
         requisite_number = requisite.split()[-1]
 
         if len(requisite_number) == 16:
-            return f"{requisite_type} {get_mask_card_number(int(requisite_number))}"
+            int_requisite = int(requisite_number)
+            return f"{requisite_type} {get_mask_card_number(int_requisite)}"
         elif len(requisite_number) == 20:
-            return f"{requisite_type} {get_mask_account(int(requisite_number))}"
+            int_requisite = int(requisite_number)
+            return f"{requisite_type} {get_mask_account(int_requisite)}"
         else:
             return "Предоставлен неизвестный тип реквизита клиента"
     else:
-        return "Предоставлены неверные данные. Проверьте правильность ввода данных"
+        return "Предоставлены неверные данные. Проверьте правильность ввода"
 
 
 def get_date(date: str) -> str:
