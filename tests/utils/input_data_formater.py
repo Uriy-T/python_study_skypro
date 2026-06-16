11befc5b29e06afcf19e8de1481ecb94b28ac98c
Uriy-T <briner_asterios@mail.ru>
2026-06-16 23:43:46 +0400

    Добавочно реализовал простые форматеры для устранения конфликта типизации в маркировке pytest.mark.parametrize().

--- tests/utils/input_data_formater.py
+++ tests/utils/input_data_formater.py
@@ -1,7 +1,12 @@
-def input_data_formater(data: list[dict[str, str]]) -> list[str | list[tuple[str]]]:
-    """Функция принимает на вход набор данных представляющий собой список словарей.
-    И переформатирует его в конструкцию, которую ожидает получить для работы фикстура
-    @pytest.mark.parametrize.
+from typing import Any, Union
+
+
+def input_data_formater(
+    data: list[dict[str, Any]],
+) -> list[Union[str, list[tuple[Any, ...]]]]:
+    """Функция принимает на вход набор данных представляющий
+    собой список словарей. И переформатирует его в конструкцию,
+    которую ожидает получить для работы фикстура @pytest.mark.parametrize.
 
     :param data: список словарей содержащих параметры ввода
     :return: список содержащий строку состоящую из имен параметров ввода
@@ -11,4 +16,12 @@ def input_data_formater(data: list[dict[str, str]]) -> list[str | list[tuple[str
     param_pack = list(data[0].keys())
     value_pack = [tuple(item.values()) for item in data]
 
-    return [', '.join(param_pack), value_pack]
+    return [", ".join(param_pack), value_pack]
+
+
+def param_packer(data: list[dict[str, Any]]) -> str:
+    return ", ".join(list(data[0].keys()))
+
+
+def value_packer(data: list[dict[str, Any]]) -> list[tuple]:
+    return [tuple(item.values()) for item in data]
