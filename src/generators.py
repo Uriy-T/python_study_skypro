b5766957788c6a551533aa5ffeab8256322cee94
Uriy-T <briner_asterios@mail.ru>
2026-06-20 13:09:37 +0400

    Реализованы функции filter_by_currency, transaction_descriptions и card_number_generator в модуле src/generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,13 +1,39 @@
-def filter_by_currency(transactions: list[dict], currency: str):
-    return (operation for operation in transactions if operation['operationAmount']['currency']['name'] == currency)
+from typing import Any, Generator
+
+from src.valodators.generators_validators import (
+    currency_validator,
+    data_type_validator,
+    interval_validator,
+    structure_validator,
+)
 
 
-def transaction_descriptions(transactions: list[dict]):
+def filter_by_currency(
+    transactions: list[dict], currency: str
+) -> Generator[dict[Any, Any], None, None]:
+    data_type_validator(transactions)
+    structure_validator(transactions)
+    currency_validator(currency)
+    return (
+        operation
+        for operation in transactions
+        if operation["operationAmount"]["currency"]["name"] == currency
+    )
+
+
+def transaction_descriptions(
+    transactions: list[dict],
+) -> Generator[dict[Any, Any], None, None]:
+    data_type_validator(transactions)
+    structure_validator(transactions)
+
     for operation in transactions:
-        yield operation['description']
+        yield operation["description"]
 
 
-def card_number_generator(start: int, end: int):
+def card_number_generator(start: int, end: int) -> Generator[str]:
+    interval_validator(start, end)
+
     for num in range(start, end + 1):
-        card_str = str(num).rjust(16, "0")  # rjust — выравнивает вправо, заполняет нулями
-        yield f"{card_str[:4]} {card_str[4:8]} {card_str[8:12]} {card_str[12:16]}"
+        card_st = str(num).rjust(16, "0")
+        yield f"{card_st[:4]} {card_st[4:8]} {card_st[8:12]} {card_st[12:16]}"
