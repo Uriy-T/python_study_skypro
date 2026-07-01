1b6e4a5e5f7d5646c56af92f87e7f891152d24b4
Uriy-T <briner_asterios@mail.ru>
2026-07-01 23:13:11 +0400

    Реализовал логгер компоненты: - Описал настройки логгера; - Установил в код функций логгеры событий.

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,22 @@
+import logging
+
+mask_card_logger = logging.getLogger("masks.get_mask_card_number")
+mask_account_logger = logging.getLogger("masks.get_mask_account")
+
+mask_handler = logging.FileHandler(
+    "logs/masks.log", encoding="utf-8", mode="w"
+)
+mask_formater = logging.Formatter(
+    "%(asctime)s-%(name)s-%(levelname)s-%(message)s"
+)
+mask_handler.setFormatter(mask_formater)
+
+mask_card_logger.addHandler(mask_handler)
+mask_card_logger.setLevel(logging.DEBUG)
+mask_account_logger.addHandler(mask_handler)
+mask_account_logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card_number: int) -> str:
     """Функция частично маскИрует номер карты клиента банка.
 
@@ -7,14 +26,22 @@ def get_mask_card_number(card_number: int) -> str:
     """
 
     if not isinstance(card_number, int) or isinstance(card_number, bool):
+        mask_card_logger.error("Переданное значение не является int")
         raise TypeError("Переданное значение не является int")
     if card_number <= 0:
+        mask_card_logger.error("Переданное число не является положительным")
         raise ValueError("Переданное число не является положительным")
 
     str_number = str(card_number)
     if len(str_number) != 16:
+        mask_card_logger.error("Номер карты должен содержать 16 цифр")
         raise ValueError("Номер карты должен содержать 16 цифр")
-    return f"{str_number[:4]} {str_number[4:6]}** **** {str_number[-4:]}"
+
+    masked_number = (
+        f"{str_number[:4]} {str_number[4:6]}** **** {str_number[-4:]}"
+    )
+    mask_card_logger.info(f"Номер карты успешно замаскирован: {masked_number}")
+    return masked_number
 
 
 def get_mask_account(account: int) -> str:
@@ -26,11 +53,18 @@ def get_mask_account(account: int) -> str:
      ****ХХХХ
     """
     if not isinstance(account, int) or isinstance(account, bool):
+        mask_account_logger.error("Переданное значение не является int")
         raise TypeError("Переданное значение не является int")
     if account <= 0:
+        mask_account_logger.error("Номер счета не может быть отрицательным")
         raise ValueError("Номер счета не может быть отрицательным")
 
     str_account = str(account)
     if len(str_account) != 20:
+        mask_account_logger.error("Номер счета должен содержать 20 цифр")
         raise ValueError("Номер счета должен содержать 20 цифр")
-    return f"****{str(account)[-4:]}"
+    masked_account = f"****{str(account)[-4:]}"
+    mask_account_logger.info(
+        f"Номер счета успешно замаскирован: {masked_account}"
+    )
+    return masked_account
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,18 @@
 import json
+import logging
 import os.path
 from typing import Any
 
-from src.validators.get_info_validators import path_type_validator
+transaction_logger = logging.getLogger("utils.get_transaction_info")
+transaction_logger.setLevel(logging.DEBUG)
+transaction_handler = logging.FileHandler(
+    "logs/utils.log", encoding="utf-8", mode="w"
+)
+transaction_formater = logging.Formatter(
+    "%(asctime)s-%(name)s-%(levelname)s-%(message)s"
+)
+transaction_handler.setFormatter(transaction_formater)
+transaction_logger.addHandler(transaction_handler)
 
 
 def get_transaction_info(path_to_data: str) -> list[dict[str, Any]]:
@@ -12,9 +22,23 @@ def get_transaction_info(path_to_data: str) -> list[dict[str, Any]]:
     :param path_to_data: принимает путь к файлу с данными для чтения
     :return: python-объект с данными об операциях
     """
-    path_type_validator(path_to_data, str)
+
+    actual_type = type(path_to_data)
+    if actual_type != str:
+        transaction_logger.error(
+            f"Переданный путь к файлу не является строкой."
+            f"передано значение типа {actual_type.__name__}"
+        )
+        raise TypeError(
+            f"Ожидаемый тип данных: str."
+            f"Передан тип: {actual_type.__name__}."
+        )
 
     if not os.path.exists(path_to_data) or os.path.getsize(path_to_data) == 0:
+        transaction_logger.error(
+            f'Файл пустой или не существует по указанному пути: '
+            f'"{path_to_data}".'
+        )
         return []
 
     with open(path_to_data, "r", encoding="utf-8") as data:
@@ -22,4 +46,7 @@ def get_transaction_info(path_to_data: str) -> list[dict[str, Any]]:
         if not isinstance(content, list):
             return []
         else:
+            transaction_logger.info(
+                f'Файл по пути "{path_to_data}" успешно обработан.'
+            )
             return content
