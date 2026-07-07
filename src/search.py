80607bf7fb9dc06e19ef39ac2d7b8ae0892a444e
Uriy-T <briner_asterios@mail.ru>
2026-07-07 20:26:59 +0400

    Доработал модуль:  - Внедрил в функцию search_by_descr валилдаторы data_type_validator и - collection_elements_type_validator для обработки исключений;
     - Добавил строку документации для функции search_by_descr.

--- src/search.py
+++ src/search.py
@@ -1,64 +1,29 @@
 import re
 from typing import Any
+from src.validators.base_validator import (
+    data_type_validator,
+    collection_elements_type_validator
+)
 
 
 def search_by_descr(
         transactions: list[dict[str, Any]],
         search_str: str
 ) -> list[dict[str, Any]]:
+    """
+    Функция фильтрует транзакции из общего списка
+    по переданной подстроке, по частичному совпадению
+    :param transactions: Принимает список словарей,
+    описывающих транзакции
+    :param search_str: Принимает подстроку для
+    фильтрации транзакций
+    :return: отфильтрованный список объектов
+    """
+    data_type_validator(transactions, list, 'transactions')
+    collection_elements_type_validator(transactions, dict, 'transactions')
+    data_type_validator(search_str, str, 'search_str')
+
     search_filter = re.compile(rf'{search_str.lower()}')
 
     return [transaction for transaction in transactions
             if re.search(search_filter, transaction['description'].lower())]
-
-
-# test_data = [
-#     {
-#         "id": 441945886,
-#         "state": "EXECUTED",
-#         "date": "2019-08-26T10:50:58.294041",
-#         "operationAmount": {
-#             "amount": "31957.58",
-#             "currency": {
-#                 "name": "руб.",
-#                 "code": "RUB"
-#             }
-#         },
-#         "description": "Пополнение счета",
-#         "from": "Maestro 1596837868705199",
-#         "to": "Счет 64686473678894779589"
-#     },
-#     {
-#         "id": 41428829,
-#         "state": "EXECUTED",
-#         "date": "2019-07-03T18:35:29.512364",
-#         "operationAmount": {
-#             "amount": "8221.37",
-#             "currency": {
-#                 "name": "USD",
-#                 "code": "USD"
-#             }
-#         },
-#         "description": "Списание по кредиту",
-#         "from": "MasterCard 7158300734726758",
-#         "to": "Счет 35383033474447895560"
-#     },
-#     {
-#         "id": 939719570,
-#         "state": "EXECUTED",
-#         "date": "2018-06-30T02:08:58.425572",
-#         "operationAmount": {
-#             "amount": "9824.07",
-#             "currency": {
-#                 "name": "EUR",
-#                 "code": "EUR"
-#             }
-#         },
-#         "description": "Списание по кредиту",
-#         "from": "Счет 75106830613657916952",
-#         "to": "Счет 11776614605963066702"
-#     }
-# ]
-#
-# print(search_by_descr(transactions=test_data,
-#                       search_str='по'))
